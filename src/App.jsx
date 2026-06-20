00eb719c1d4b5d71c5884406d3495b986d095d6c
ondrejpupik <onpupik@gmail.com>
2026-06-20 19:48:23 +0200

    code cleanup and improvements (no braking changes)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import artbovich from "./assets/artbovich.jpg";
-import beratilgin from "./assets/beratilgin.jpg";
-import theGhazi from "./assets/the-ghazi.jpg";
+import artbovich from "./assets/artbovich.webp";
+import beratilgin from "./assets/beratilgin.webp";
+import theGhazi from "./assets/the-ghazi.webp";
 
 function App() {
   return (
-    <section className="max-w-4xl mx-auto p-4 pt-6">
+    <main className="mx-auto max-w-4xl p-4 pt-6">
       <div>
         <div className="lg:flex lg:flex-row">
-          <span className="w-full lg:w-[42%] lg:text-xs font-medium text-gray-500 uppercase">
+          <span className="font-medium text-gray-500 uppercase lg:w-[42%] lg:text-xs">
             vize projektu
           </span>
-          <div className="lg:relative w-full lg:w-[58%] pt-2">
-            <h2 className="font-medium text-4xl lg:text-5xl leading-tight uppercase">
+          <div className="pt-2 lg:relative lg:w-[58%]">
+            <h2 className="text-4xl leading-tight font-medium uppercase lg:text-5xl">
               harmonie v každém detailu
             </h2>
-            <p className="lg:absolute lg:top-full lg:left-0 w-full  text-xl lg:text-base leading-relaxed text-gray-800 pb-8 pt-7">
+            <p className="pt-7 pb-8 text-xl leading-relaxed text-gray-800 lg:absolute lg:top-full lg:left-0 lg:text-base">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
               esse iste odit. Adipisci labore, repellendus accusantium obcaecati
               saepe exercitationem illo ut dicta earum delectus, dolore nulla,
@@ -23,12 +23,12 @@ function App() {
           </div>
         </div>
 
-        <div className="w-full grid grid-cols-2 gap-5 lg:pt-9">
-          <div className="flex flex-col gap-6 justify-between pb-4 lg:pr-5">
+        <div className="grid w-full grid-cols-2 gap-5 lg:pt-9">
+          <div className="flex flex-col justify-between gap-6 pb-4 lg:pr-5">
             <img
               src={beratilgin}
               alt="Krásná zahrádka"
-              className="w-[80%] aspect-square rounded-lg"
+              className="aspect-square w-[80%] rounded-lg"
             />
             <div className="w-[70%] self-end lg:py-5">
               <img
@@ -43,12 +43,12 @@ function App() {
             <img
               src={artbovich}
               alt="Moderní interiér jídelny"
-              className="w-full aspect-[5/6] object-cover rounded-lg"
+              className="aspect-5/6 w-full rounded-lg object-cover"
             />
           </div>
         </div>
       </div>
-    </section>
+    </main>
   );
 }
 
