c40e188f346e3bf0cb1fc02411fe887904ad7bde
ondrejpupik <onpupik@gmail.com>
2026-06-20 16:26:04 +0200

    Styling for mobile devices done

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,43 @@ import theGhazi from "./assets/the-ghazi.jpg";
 
 function App() {
   return (
-    <section>
-      <span className="text-gray-400 uppercase">vize projektu</span>
-      <h2 className="font-bold text-lg uppercase">harmonie v každém detailu</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore esse
-        iste odit. Adipisci labore, repellendus accusantium obcaecati saepe
-        exercitationem illo ut dicta earum delectus, dolore nulla, laboriosam
-        neque animi vel!
-      </p>
-      <img src={beratilgin} alt="Krásná zahrádka" />
-      <img src={theGhazi} alt="Moderní vila" />
-      <img src={artbovich} alt="Moderní interiér jídelny" />
+    <section className="max-w-5xl mx-auto p-4 pt-6">
+      <div className="grid lg:grid-cols-2 gap-4 gap-6">
+        <span className="font-medium text-gray-500 uppercase">
+          vize projektu
+        </span>
+        <h2 className="font-medium text-4xl uppercase">
+          harmonie v každém detailu
+        </h2>
+        <p className="text-xl leading-relaxed text-gray-800">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore esse
+          iste odit. Adipisci labore, repellendus accusantium obcaecati saepe
+          exercitationem illo ut dicta earum delectus, dolore nulla, laboriosam
+          neque animi vel!
+        </p>
+
+        <div className="relative w-full grid grid-cols-2 gap-10">
+          <div className="flex flex-col gap-6 pb-5">
+            <div>
+              <img
+                src={beratilgin}
+                alt="Krásná zahrádka"
+                className="w-[80%] aspect-square rounded-lg"
+              />
+            </div>
+            <img
+              src={theGhazi}
+              alt="Moderní vila"
+              className="w-[70%] ml-auto aspect-square object-cover rounded-lg items-end"
+            />
+          </div>
+          <img
+            src={artbovich}
+            alt="Moderní interiér jídelny"
+            className="absolute bottom-0 right-0 w-1/2 aspect-5/6 object-cover rounded-lg"
+          />
+        </div>
+      </div>
     </section>
   );
 }
